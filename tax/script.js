2701c0d9f0be2c373f134a1e2594cca698283ce7
AskinAnsari <153223490+AskinAnsari@users.noreply.github.com>
2023-12-07 23:39:40 +0530

    Add files via upload

---
+++ tax/script.js
@@ -0,0 +1,16 @@
+function calculateTax() {
+    const income = parseFloat(document.getElementById('income').value);
+    const deductions = parseFloat(document.getElementById('deductions').value);
+
+    if (isNaN(income) || isNaN(deductions)) {
+        alert("Please enter valid numbers for income and deductions.");
+        return;
+    }
+
+    const taxableIncome = income - deductions;
+    const taxRate = 0.2; // Assuming a simple flat tax rate of 20%
+    const taxAmount = taxableIncome * taxRate;
+
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = `Your calculated tax is: $${taxAmount.toFixed(2)}`;
+}
